645e32fef088a4a0f07868864ba852aeb8901bce
Jesse Panganiban <me@jpanganiban.com>
2012-09-08 03:45:15 +0800

    Awesome

--- chan/web/controllers/common.py
+++ chan/web/controllers/common.py
@@ -14,7 +14,7 @@ def board(board):
 
 @app.route('/<board>/<thread_id>')
 def board_thread(board, thread_id):
-    thread = Threads(board, thread_id)
+    thread = objects.Thread(board, thread_id)
     if not thread:
         abort(404)
     return jsonify(thread.to_dict())
